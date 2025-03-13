ccadb2879e80cf056fd8f0d8bfbe5066b9b5ce4e
mhowai0802 <m.howai_stuff@yahoo.com.hk>
2025-03-13 11:05:23 +0800

    change_name_webserver

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ python main.py &
 
 # Start Flask on the port specified by Render
 # The --no-debugger and --no-reload flags disable development features
-export FLASK_APP=web_server.py
+export FLASK_APP=webserver.py
 flask run --host=0.0.0.0 --port=${PORT:-10000} --no-debugger --no-reload
