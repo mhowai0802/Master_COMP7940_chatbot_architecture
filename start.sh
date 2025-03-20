284a27885fc4d5841356ddeab0ca3d81835ddb7f
mhowai0802 <m.howai_stuff@yahoo.com.hk>
2025-03-20 09:42:54 +0800

    delete_files

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Start the bot in the background
-python main.py &
-
-# Start Flask on the port specified by Render
-# The --no-debugger and --no-reload flags disable development features
-export FLASK_APP=webserver.py
-flask run --host=0.0.0.0 --port=${PORT:-8080} --no-debugger --no-reload
